fcde3dcb7f057407c146451b4178dd614a89f813
jeffolsen <olsenjeffn@gmail.com>
2025-11-17 14:20:18 -0500

    separate conscerns for auth and profile routes

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import authApi from "../controllers/auth";
+
+const router = express.Router();
+
+router.post("/register", authApi.register);
+router.post("/login", authApi.login);
+router.post("/logout", authApi.logout);
+router.post("/logout-all", authApi.logoutOfAll);
+
+export default router;
--- backend/src/routes/profiles.ts
+++ backend/src/routes/profiles.ts
@@ -3,12 +3,9 @@ import profileApi from "../controllers/profile";
 
 const router = express.Router();
 
-router.post("/register", profileApi.register);
-router.post("/login", profileApi.login);
-router.post("/logout", profileApi.logout);
-router.post("/logout-all", profileApi.logoutOfAll);
+router.get("/me", profileApi.getAuthenticatedProfile);
 router.post("/change-password", profileApi.changePassword);
 router.post("/verify-email", profileApi.verifyEmail);
-router.post("/delete", profileApi.deleteProfile);
+router.delete("/me", profileApi.deleteProfile);
 
 export default router;
