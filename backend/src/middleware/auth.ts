380eb0a3b5b26dc78dcfe4524820f670067ade50
jeffolsen <olsenjeffn@gmail.com>
2025-11-17 14:21:10 -0500

    add auth middleware and use it to block profile routes

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,30 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import env from "../util/env";
 
-export const requiresAuth: RequestHandler = (req, res, next) => {
-  if (req?.session?.userId) next();
-  else next(createHttpError(401, "User not authenticated"));
+interface AccessTokenPayload {
+  profileId: string;
+  sessionId: string;
+}
+
+const requiresAuth: RequestHandler = async (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+    if (!accessToken) throw createHttpError(401, "Unauthorized");
+
+    const payload = jwt.verify(
+      accessToken,
+      env.JWT_SECRET
+    ) as AccessTokenPayload;
+    if (!payload) throw createHttpError(401, "Unauthorized");
+
+    req.body.profileId = payload.profileId;
+    req.body.sessionId = payload.sessionId;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
+
+export default requiresAuth;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import profileRoutes from "./routes/profiles";
+import authRoutes from "./routes/auth";
+import requiresAuth from "./middleware/auth";
 import handleError, { endPointNotFound } from "./middleware/handleError";
 import env from "./util/env";
 import cors from "cors";
@@ -11,7 +13,8 @@ const PORT = env.PORT || 5001;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/profiles", profileRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profiles", requiresAuth, profileRoutes);
 
 app.use(endPointNotFound);
 app.use(handleError);
