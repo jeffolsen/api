7f8bd48c8177df128feb6abff588091a7815c42d
jeffolsen <olsenjeffn@gmail.com>
2025-12-01 17:25:46 -0500

    WIP utils
    - Add bcrypt wrapper
    - Add scope utils
    - Fix date construction
    - Add cookie defaults

--- backend/src/util/cookie.ts
+++ backend/src/util/cookie.ts
@@ -1,12 +1,24 @@
 import { session } from "../db/client";
 import { CookieOptions, Response } from "express";
 import { getNewAccessTokenExpirationDate } from "./date";
+import env from "../config/env";
+
+const secure = env.NODE_ENV !== "development";
+
+const defaults: CookieOptions = {
+  sameSite: "none",
+  secure,
+};
 
 const getAccessTokenCookieOptions = (): CookieOptions => {
-  return { expires: getNewAccessTokenExpirationDate() };
+  return {
+    ...defaults,
+    expires: getNewAccessTokenExpirationDate(),
+  };
 };
 const getRefreshTokenCookieOptions = (expiresAt: Date): CookieOptions => {
   return {
+    ...defaults,
     expires: expiresAt,
   };
 };
--- backend/src/util/date.ts
+++ backend/src/util/date.ts
@@ -4,20 +4,20 @@ import {
 } from "../config/constants";
 
 export const getNewAccessTokenExpirationDate = (): Date =>
-  new Date(new Date().getDate() + ACCESS_TOKEN_LIFESPAN);
+  new Date(Date.now() + ACCESS_TOKEN_LIFESPAN);
 
 export const getNewRefreshTokenExpirationDate = (): Date =>
-  new Date(new Date().getDate() + SESSION_TOKEN_LIFESPAN);
+  new Date(Date.now() + SESSION_TOKEN_LIFESPAN);
 
-export const beforeNow = (date: Date): boolean => {
-  return date.getDate() < new Date().getDate();
+export const beforeNow = (date: string | Date): boolean => {
+  return new Date(date).getTime() < Date.now();
 };
 
-export const afterNow = (date: Date): boolean => {
+export const afterNow = (date: string | Date): boolean => {
   return !beforeNow(date);
 };
 
-const D = (date: Date) => {
+const D = (date: string | Date) => {
   return {
     isBeforeNow: () => beforeNow(date),
     isAfterNow: () => afterNow(date),
