c705232139cc9e320cf5e1b0a91d5b79cb244b70
jeffolsen <olsenjeffn@gmail.com>
2026-02-12 13:27:36 -0500

    cleanup login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,23 +1,10 @@
-import LoginForm from "../components/forms/LoginForm";
-import RegisterForm from "../components/forms/RegisterForm";
-import VerificationCodeForm from "../components/forms/VerificationCodeForm";
-import Wrapper from "../components/common/Wrapper";
-import Heading from "../components/common/Heading";
+import LoginOrRegisterBlock from "../components/blocks/LoginOrRegisterBlock";
 
 function LoginPage() {
   return (
-    <Wrapper width="sm">
-      <Heading
-        headingSize="lg"
-        headingStyles={"uppercase font-bold text-rimary-content"}
-        headingDecorator="underline"
-      >
-        Should be level 1
-      </Heading>
-      <LoginForm />
-      <RegisterForm />
-      <VerificationCodeForm />
-    </Wrapper>
+    <>
+      <LoginOrRegisterBlock title="Welcome" isprimaryContent />
+    </>
   );
 }
 
