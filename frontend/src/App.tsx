6b13b380554eb9f7cee798d18aac9e95baed06e7
jeffolsen <olsenjeffn@gmail.com>
2026-02-15 20:53:23 -0500

    add react-cookie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,21 @@ import { Route, Routes } from "react-router";
 // import LoginPage from "./pages/LoginPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import GenericPage from "./pages/GenericPage";
+import { CookiesProvider } from "react-cookie";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Routes>
-          {/* <Route path="/" element={<LoginPage />} /> */}
-          <Route path="*" element={<GenericPage />} />
-        </Routes>
-      </Layout>
-    </QueryClientProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Routes>
+            <Route path="*" element={<GenericPage />} />
+          </Routes>
+        </Layout>
+      </QueryClientProvider>
+    </CookiesProvider>
   );
 }
 
