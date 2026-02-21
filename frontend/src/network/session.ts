ab83f9f690352905808813bd9833d2baec5484ee
jeffolsen <olsenjeffn@gmail.com>
2026-02-21 17:58:36 -0500

    add logout functions

--- frontend/src/network/session.ts
+++ frontend/src/network/session.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { SESSIONS_ENDPOINT } from "./api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { SESSIONS_ENDPOINT, LOGOUT_ENDPOINT } from "./api";
 import { useAuthState } from "../contexts/AuthContext";
 
 const SESSION_KEY = "sessions" as const;
@@ -18,13 +18,43 @@ export const useGetProfilesSessions = () => {
   return query;
 };
 
-export const logout = async () => {};
+export const useLogout = () => {
+  const { api } = useAuthState();
+
+  return useMutation({
+    mutationFn: async () => {
+      const response = await api.post(LOGOUT_ENDPOINT);
+      return response.data;
+    },
+    onSuccess: () => {
+      // setIsAuthenticated(false);
+      console.log("congrats you logged out");
+    },
+    onError: (error) => {
+      console.error("useLogout", error);
+    },
+  });
+};
 
 interface LogoutAllInput {
   verificationCode: string;
   email: string;
 }
 
-export const logoutAll = async (input: LogoutAllInput) => {
-  return input;
+export const useLogoutAll = () => {
+  const { api } = useAuthState();
+
+  return useMutation({
+    mutationFn: async (data: LogoutAllInput) => {
+      const response = await api.post(LOGOUT_ENDPOINT, data);
+      return response.data;
+    },
+    onSuccess: () => {
+      // setIsAuthenticated(false);
+      console.log("congrats you logged out of all sessions");
+    },
+    onError: (error) => {
+      console.error("useLogoutAll", error);
+    },
+  });
 };
