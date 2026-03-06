50e1f6b01b533e18dc7f60179415c71f8d47a605
jeffolsen <olsenjeffn@gmail.com>
2026-03-06 09:42:21 -0500

    implement breadcrumbs

---
+++ frontend/src/components/layout/BreadCrumbs.tsx
@@ -0,0 +1,42 @@
+import { useLocation } from "react-router";
+import Link from "../common/Link";
+import Wrapper from "../common/Wrapper";
+
+function BreadCrumbs() {
+  const location = useLocation();
+  const path = location.pathname.replace(/^\/|\/$/g, "");
+  const pathSegments = path.split("/").filter(Boolean);
+  const crumbs = ["home"];
+  let accumulatedPath = "";
+
+  pathSegments.forEach((_, index) => {
+    accumulatedPath = "";
+    pathSegments.forEach((seg, segIndex) => {
+      if (segIndex > index) return;
+      if (accumulatedPath) {
+        accumulatedPath += `/${seg}`;
+      } else {
+        accumulatedPath = seg;
+      }
+    });
+    crumbs.push(accumulatedPath);
+  });
+
+  return (
+    <Wrapper width="lg">
+      <div className="breadcrumbs text-sm w-full">
+        <ul>
+          {crumbs.map((crumb, index) => (
+            <li key={index} className="capitalize">
+              <Link to={`/${crumb === "home" ? "" : crumb}`}>
+                {crumb.replaceAll("/", ` ${String.fromCharCode(8226)} `)}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default BreadCrumbs;
--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,13 +2,15 @@ import { PropsWithChildren } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { HeadingLevelProvider } from "../common/Heading";
+import BreadCrumbs from "./BreadCrumbs";
 
 function Layout({ children }: PropsWithChildren) {
   return (
     <HeadingLevelProvider>
       <div className="min-h-screen flex flex-col justify-between bg-base-100">
         <Header />
-        <main className="flex-grow flex flex-col gap-8 items-center justify-center py-8">
+        <main className="flex-grow flex flex-col gap-6 items-center justify-center py-6">
+          <BreadCrumbs />
           {children}
         </main>
         <Footer />
