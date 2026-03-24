cf8bd4b38a0abb5b4b49e11d834aefd455981568
jeffolsen <olsenjeffn@gmail.com>
2026-03-24 06:57:25 -0400

    Handle empty string in search param

--- frontend/src/hooks/useSearchParam.ts
+++ frontend/src/hooks/useSearchParam.ts
@@ -5,7 +5,7 @@ export const useSearchParam = (key: string) => {
   const value = searchParams.get(key) || undefined;
 
   const setValue = (newValue: string | undefined) => {
-    if (newValue === undefined) {
+    if (newValue === undefined || newValue === "") {
       searchParams.delete(key);
     } else {
       searchParams.set(key, newValue);
@@ -24,7 +24,11 @@ export const useSearchParamWithDefault = (
   const value = searchParams.get(key) || defaultValue;
 
   const setValue = (newValue: string | undefined) => {
-    if (newValue === undefined) {
+    if (
+      newValue === undefined ||
+      newValue === defaultValue ||
+      newValue === ""
+    ) {
       searchParams.delete(key);
     } else {
       searchParams.set(key, newValue);
@@ -33,4 +37,35 @@ export const useSearchParamWithDefault = (
   };
 
   return [value, setValue];
+};
+
+export const useSearchParamsWithDefaults = (
+  defaults: Record<string, string>,
+): [
+  Record<string, string>,
+  (key: string, newValue: string | undefined) => void,
+] => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const values = Object.fromEntries(
+    Object.entries(defaults).map(([key, defaultValue]) => [
+      key,
+      searchParams.get(key) || defaultValue,
+    ]),
+  );
+
+  const setValue = (key: string, newValue: string | undefined) => {
+    if (
+      newValue === undefined ||
+      newValue === defaults[key] ||
+      newValue === ""
+    ) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, newValue);
+    }
+    setSearchParams(searchParams);
+  };
+
+  return [values, setValue];
 };
