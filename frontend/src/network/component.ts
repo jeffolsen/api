c39d33f15edcfcde34683e39509d1d2609acf19f
jeffolsen <olsenjeffn@gmail.com>
2026-04-02 08:52:54 -0400

    Frontend component fetchers
    - Update component and component type fetchers/types to reflect backend changes.

--- frontend/src/network/component.ts
+++ frontend/src/network/component.ts
@@ -1,7 +1,56 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { TComponentType } from "./componentType";
 import { TFeed } from "./feed";
+import { useAuthState } from "../contexts/AuthContext";
+import { COMPONENTS_ENDPOINT, withErrorHandling } from "./api";
+import { FEEDS_KEY } from "./feed";
 
 export const COMPONENTS_KEY = "components" as const;
+
+export const useCreateComponent = () => {
+  const { api } = useAuthState();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: TComponentInput) =>
+      withErrorHandling(async () => {
+        console.log("Creating component with data:", data);
+        const response = await api.post(COMPONENTS_ENDPOINT, data);
+        return response.data;
+      }),
+    onSuccess: (_, { feedId }) => {
+      queryClient.invalidateQueries({
+        queryKey: [FEEDS_KEY, feedId, COMPONENTS_KEY],
+      });
+    },
+  });
+};
+
+export type ModifyComponentRequest = Partial<TComponentInput> & {
+  feedId: TFeed["id"];
+};
+
+export const useModifyComponent = (componentId: number) => {
+  const { api } = useAuthState();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: ModifyComponentRequest) =>
+      withErrorHandling(async () => {
+        console.log("Modifying component with data:", data);
+        const response = await api.patch(
+          `${COMPONENTS_ENDPOINT}/${componentId}`,
+          data,
+        );
+        return response.data;
+      }),
+    onSuccess: (_, { feedId }) => {
+      queryClient.invalidateQueries({
+        queryKey: [FEEDS_KEY, feedId, COMPONENTS_KEY],
+      });
+    },
+  });
+};
 
 export type TComponent = {
   id: number;
@@ -9,8 +58,8 @@ export type TComponent = {
   feedId: TFeed["id"];
   order: number;
   propertyValues: Record<string, unknown>;
-  publishedAt?: string;
-  expiredAt?: string;
+  publishedAt?: string | null;
+  expiredAt?: string | null;
   createdAt: string;
   updatedAt: string;
 };
--- frontend/src/network/componentType.ts
+++ frontend/src/network/componentType.ts
@@ -1,19 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryOptions, useQuery } from "@tanstack/react-query";
 import { COMPONENT_TYPES_ENDPOINT } from "./api";
 import { useAuthState } from "../contexts/AuthContext";
 import { TSubjectType } from "./feed";
 
 export const COMPONENT_TYPES_KEY = "componentsTypes" as const;
 
-export const useGetComponentTypes = () => {
+export type TComponentTypesQueryParams = {
+  subjectType?: TSubjectType;
+};
+
+export type GetComponentTypesResponse = {
+  componentTypes: TComponentType[];
+};
+
+export const useGetComponentTypes = (
+  queryParams?: TComponentTypesQueryParams,
+  options?: QueryOptions,
+) => {
   const { api } = useAuthState();
 
   return useQuery({
-    queryKey: [COMPONENT_TYPES_KEY],
-    queryFn: async () => {
-      const response = await api.get(COMPONENT_TYPES_ENDPOINT);
+    queryKey: [COMPONENT_TYPES_KEY, queryParams],
+    queryFn: async (): Promise<GetComponentTypesResponse> => {
+      const response = await api.get(COMPONENT_TYPES_ENDPOINT, {
+        params: queryParams,
+      });
       return response.data;
     },
+    ...options,
   });
 };
 
@@ -28,5 +42,4 @@ export type TComponentType = {
   name: TComponentNames;
   subjectType: TSubjectType;
   propertySchema: Record<string, unknown>;
-  updatedAt: string;
 };
