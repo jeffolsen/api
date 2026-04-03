2736874539e02e3d503b1351841b2f3be83e2efe
jeffolsen <olsenjeffn@gmail.com>
2026-04-03 08:06:19 -0400

    wip - radioinput

---
+++ frontend/src/components/inputs/RadioInput.tsx
@@ -0,0 +1,62 @@
+import clsx from "clsx";
+import {
+  AtomicFormComponentProps,
+  ChildFromFormProps,
+  RequiredLabel,
+  FormError,
+  SelectableOption,
+} from "./Input";
+
+export type RadioInputProps = Omit<
+  AtomicFormComponentProps & ChildFromFormProps,
+  "control" | "rules" | "componentName"
+>;
+
+export const RadioInput = (props: RadioInputProps) => {
+  const { dataName, displayName, register, watch, errors, input } = props;
+  const required = !!input?.registerOpts?.required;
+  const valueOptions = input?.valueOptions || [];
+  const watchedValue = watch(dataName);
+
+  return (
+    <>
+      <fieldset
+        className={clsx([
+          "form-control flex flex-row flex-wrap gap-4 border rounded p-4 pl-6 border-base-content/20",
+        ])}
+      >
+        <legend
+          className={clsx([
+            "label-text text-sm font-semibold text-neutral-content/70 w-full float-start",
+          ])}
+        >
+          <span>
+            <RequiredLabel
+              watchedValue={watchedValue}
+              required={required}
+              position="absolute"
+            />
+            {displayName}
+          </span>
+        </legend>
+        <div className="flex flex-row gap-4">
+          {valueOptions?.map((option: SelectableOption) => (
+            <label key={option.value}>
+              <input
+                className="radio"
+                {...register(dataName)}
+                type="radio"
+                value={option.value}
+                defaultChecked={option.default}
+              />
+              {option.label}
+            </label>
+          ))}
+        </div>
+      </fieldset>
+      <FormError error={errors} />
+    </>
+  );
+};
+
+export default RadioInput;
