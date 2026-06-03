aead04af37d55862ea390536236787e209218773
jeffolsen <olsenjeffn@gmail.com>
2026-06-03 14:05:04 -0400

    fix apect ratios

--- frontend/src/components/common/helpers/aspectRatioStyles.ts
+++ frontend/src/components/common/helpers/aspectRatioStyles.ts
@@ -1,11 +1,11 @@
 export const aspectRatios = {
   none: "w-full h-full",
   natural: "w-full h-auto",
-  ".5": "w-full pb-[50%]",
-  ".66": "w-full pb-[66%]",
+  ".5": "w-full pb-[200%]",
+  ".66": "w-full pb-[150%]",
   "1": "w-full pb-[100%]",
-  "1.5": "w-full pb-[150%]",
-  "2": "w-full pb-[200%]",
+  "1.5": "w-full pb-[66%]",
+  "2": "w-full pb-[50%]",
 };
 
 export type AspectRatio = keyof typeof aspectRatios;
