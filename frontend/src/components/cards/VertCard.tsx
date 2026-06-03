80af56a126b8ecd0067033a78255c85ac4fd7d45
jeffolsen <olsenjeffn@gmail.com>
2026-06-03 14:03:37 -0400

    add vertical card

---
+++ frontend/src/components/cards/VertCard.tsx
@@ -0,0 +1,40 @@
+import ScrollInFade from "@/components/common/ScrollInFade";
+import clsx, { ClassValue } from "clsx";
+import { ReactNode } from "react";
+
+type VertCardProps = {
+  passage?: ReactNode;
+  imagery?: ReactNode;
+  overLay?: ReactNode;
+  underLay?: ReactNode;
+  reverseY?: boolean;
+  className?: ClassValue;
+};
+
+const VertCard = ({
+  passage,
+  imagery,
+  overLay,
+  underLay,
+  reverseY = false,
+  className,
+}: VertCardProps) => {
+  return (
+    <ScrollInFade className={clsx(["relative group h-full"])}>
+      <div
+        className={clsx([
+          "card w-full overflow-clip",
+          className,
+          reverseY && "flex-col-reverse",
+        ])}
+      >
+        {imagery && <figure className={"w-full"}>{imagery}</figure>}
+        {passage && <div className={"card-body w-full"}>{passage}</div>}
+      </div>
+      {overLay && <div className="absolute inset-0 z-10">{overLay}</div>}
+      {underLay && <div className="absolute inset-0 -z-10">{underLay}</div>}
+    </ScrollInFade>
+  );
+};
+
+export default VertCard;
